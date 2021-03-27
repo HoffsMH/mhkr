153423c2b5502233300482fa450ca67b604b0481
HoffsMH <matthecker@pm.me>
2021-03-26 22:14:12 -0500

    ok

--- dist/euphrates/arch/chroot.sh
+++ dist/euphrates/arch/chroot.sh
@@ -6,8 +6,10 @@ set -e
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
 
+sudo echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+sudo echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+sudo echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 localectl set-keymap --no-convert us
 loadkeys us
