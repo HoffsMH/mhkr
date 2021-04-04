72cbac3c7df88b88198d348ba1ad2a6bf8241b1f
HoffsMH <matthecker@pm.me>
2021-04-03 23:20:18 -0500

    .

---
+++ dist/euphrates/manjaro-prep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+
+mkswap /dev/nvme0n1p3 || echo "mkswap failed"
+swapon /dev/nvme0n1p3 || echo "swapon failed"
+
+echo "###############################################"
+echo "disk password"
+echo "###############################################"
+cryptsetup luksFormat /dev/nvme0n1p4
+cryptsetup open /dev/nvme0n1p4 cryptroot
+
+mkfs.btrfs /dev/mapper/cryptroot
+mkfs.vfat -F 32 /dev/nvme0n1p1
+mkfs.vfat -F 32 /dev/nvme0n1p2
+
+mount /dev/mapper/cryptroot /mnt
+mkdir -p /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
+mkdir -p /mnt/boot/efi
+mount /dev/nvme0n1p2 /mnt/boot/efi
+
+sudo pacman-mirrors -f
