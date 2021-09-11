9c629f0622bc67cc95b9e50a96b41c8678a80d1f
HoffsMH <matthecker@pm.me>
2021-09-11 00:09:37 -0500

    export

--- dist/euphrates.sh
+++ dist/euphrates.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-root_partition=/dev/nvme0n1p4
-boot_partition=/dev/nvme0n1p1
-efi_partition=/dev/nvme0n1p2
-swap=/dev/nvme0n1p3
-machine_hostname="euphrates"
+export root_partition=/dev/nvme0n1p4
+export boot_partition=/dev/nvme0n1p1
+export efi_partition=/dev/nvme0n1p2
+export swap=/dev/nvme0n1p3
+export machine_hostname="euphrates"
 
 curl https://mhkr.io/prep.sh > prep.sh
 chmod +x prep.sh
