2c787939493cbec9665820a048e71bff71cb68f9
HoffsMH <matthecker@pm.me>
2022-07-31 01:01:11 -0500

    typo

--- public/run/test-machine.sh
+++ public/run/test-machine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-export root_partition=/dev/sda4
+export root_partition=/dev/sda3
 export boot_partition=/dev/sda1
 export swap=/dev/sda2
 export machine_hostname="test-machine"
