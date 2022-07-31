ca87b50cc8559af8e508dbef12207972ac77f0aa
HoffsMH <matthecker@pm.me>
2022-07-31 00:55:39 -0500

    test machine

---
+++ public/run/test-machine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+export root_partition=/dev/sda4
+export boot_partition=/dev/sda1
+export swap=/dev/sda2
+export machine_hostname="test-machine"
+
+curl https://mhkr.xyz/run/prep.sh > prep.sh
+chmod +x prep.sh
+
+./prep.sh
