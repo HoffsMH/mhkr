4e107f944af3ca5ad5ee9fa3512fcdbf517ec69f
HoffsMH <matthecker@pm.me>
2022-08-05 23:32:20 -0500

    ok

--- static/run/test-machine.sh
+++ static/run/test-machine.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-export root_partition=/dev/sda3
+export root_partition=/dev/sda4
 export boot_partition=/dev/sda1
-export swap=/dev/sda2
+export swap=/dev/sda3
 export machine_hostname="test-machine"
 
 curl https://mhkr.xyz/run/prep.sh > prep.sh
