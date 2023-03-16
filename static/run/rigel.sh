0b9ff58a87feb9bf09f7c39d9e20382eb5030f7e
HoffsMH <matthecker@pm.me>
2023-03-16 16:50:33 -0500

    ok

--- static/run/rigel.sh
+++ static/run/rigel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set -e
-export root_partition=/dev/nvme0n1p7
-export boot_partition=/dev/nvme0n1p8
-export efi_partition=/dev/nvme0n1p9
-export swap=/dev/nvme0n1p10
+export root_partition=/dev/nvme0n1p8
+export boot_partition=/dev/nvme0n1p5
+export efi_partition=/dev/nvme0n1p6
+export swap=/dev/nvme0n1p7
 export machine_hostname="rigel"
 
 curl https://mhkr.xyz/run/prep.sh > prep.sh
