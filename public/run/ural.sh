70df9cff54e74de368e53effe7739151eeb47436
HoffsMH <matthecker@pm.me>
2023-05-02 13:42:53 -0500

    ural

---
+++ public/run/ural.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+export root_partition=/dev/nvme0n1p4
+export boot_partition=/dev/nvme0n1p1
+export efi_partition=/dev/nvme0n1p2
+export swap=/dev/nvme0n1p3
+export machine_hostname="ural"
+
+curl https://mhkr.xyz/run/prep.sh > prep.sh
+chmod +x prep.sh
+
+./prep.sh
